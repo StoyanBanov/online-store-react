d68c6ad8adc6aca083be8c696c40cbc8b47d658e
StoyanBanov <sbanov05@gmail.com>
2023-10-02 15:12:32 +0300

    add test for catalog items

--- src/components/catalog/Catalog.test.js
+++ src/components/catalog/Catalog.test.js
@@ -1,46 +1,74 @@
 import React from 'react'
 import { rest } from 'msw'
 import { setupServer } from 'msw/node'
-import { render, screen, waitFor } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import '@testing-library/jest-dom'
-import { BrowserRouter, MemoryRouter, Route, Routes } from 'react-router-dom'
+import { MemoryRouter, Route, Routes } from 'react-router-dom'
 import { Catalog } from './Catalog'
 import { DimensionsContext } from '../common/context/DimensionsContext'
 import { AuthContext } from '../common/context/AuthContext'
+import { CartContext } from '../common/context/CartContext'
 
 const mockUser = {
     _id: 1,
     roles: ['user']
 }
 
-const mockParentCategories = [
+const mockCategories = [
     {
-        _id: 1,
+        _id: '1',
         title: 'clickMe',
         childCategories: [
             {
-                _id: 1,
-                parentCategory: 1,
+                _id: '3',
+                parentCategory: '1',
                 title: 'a1cat'
             }, {
-                _id: 2,
-                parentCategory: 1,
+                _id: '4',
+                parentCategory: '1',
                 title: 'a2cat'
             }
         ]
     }, {
-        _id: 2,
-        title: 'b'
+        _id: '2',
+        title: 'b',
+        childCategories: []
+    }, {
+        _id: '3',
+        parentCategory: '1',
+        title: 'a1cat',
+        childCategories: []
+    }, {
+        _id: '4',
+        parentCategory: '1',
+        title: 'a2cat',
+        childCategories: []
+    }
+]
+
+const mockItems = [
+    {
+        _id: '1',
+        category: '3',
+        title: 'title1',
+        rating: 2,
+        price: 1,
+        images: []
     }
 ]
 
 const host = 'http://localhost:3030'
 const server = setupServer(
-    rest.get(host + `/category/:id`, (req, res, ctx) => {
-        return res(ctx.json(mockParentCategories[0]))
+    rest.get(host + `/category/:cId`, (req, res, ctx) => {
+        return res(ctx.json(mockCategories.find(c => c._id === req.params.cId)))
     }),
     rest.get(host + `/category?where=${encodeURIComponent('parentCategory=null')}`, (req, res, ctx) => {
-        return res(ctx.json(mockParentCategories))
+        return res(ctx.json(mockCategories))
+    }),
+    rest.get(host + `/item`, (req, res, ctx) => {
+        let where = Object.fromEntries(req.url.searchParams.get('where').split('&').map(q => q.split('=').map((a, i) => i === 1 ? JSON.parse(a) : a)))
+
+        return res(ctx.json(mockItems.filter(i => i.category === where.category)))
     })
 )
 
@@ -51,29 +79,39 @@ afterAll(() => server.close())
 test('loads parent categories', async () => {
     renderSkeleton(mockUser)
 
-    const titles = await Promise.all(mockParentCategories.map(c => screen.findByText(c.title)))
+    const titles = await Promise.all(mockCategories.map(c => screen.findByText(c.title)))
 
-    expect(titles.length).toBe(mockParentCategories.length)
+    expect(titles.length).toBe(mockCategories.length)
 })
 
 test('loads child categories', async () => {
-    renderSkeleton(mockUser, `/${mockParentCategories[0].title}/${mockParentCategories[0]._id}`)
+    renderSkeleton(mockUser, `/${mockCategories[0].title}/${mockCategories[0]._id}`)
 
-    let titles = await Promise.all(mockParentCategories[0].childCategories.map(c => screen.findByText(c.title)))
+    let titles = await Promise.all(mockCategories[0].childCategories.map(c => screen.findByText(c.title)))
 
-    expect(titles.length).toBe(mockParentCategories[0].childCategories.length)
+    expect(titles.length).toBe(mockCategories[0].childCategories.length)
+})
+
+test('loads items', async () => {
+    renderSkeleton(mockUser, `/${mockCategories[2].title}/${mockCategories[2]._id}`)
+
+    let titles = await Promise.all(mockItems.map(c => screen.findByText(c.title)))
+
+    expect(titles.length).toBe(mockItems.length)
 })
 
 function renderSkeleton(user, route = '') {
     render(
         <MemoryRouter initialEntries={[`/catalog${route}`]}>
             <DimensionsContext.Provider value={{ windowWidth: 1000 }}>
-                <AuthContext.Provider value={{ user }}>
-                    <Routes>
-                        <Route path='catalog/:catTitle/:catId' element={<Catalog />} />
-                        <Route path='catalog' element={<Catalog />} />
-                    </Routes>
-                </AuthContext.Provider>
+                <CartContext.Provider value={{ addToCart: () => { } }}>
+                    <AuthContext.Provider value={{ user }}>
+                        <Routes>
+                            <Route path='catalog/:catTitle/:catId' element={<Catalog />} />
+                            <Route path='catalog' element={<Catalog />} />
+                        </Routes>
+                    </AuthContext.Provider>
+                </CartContext.Provider>
             </DimensionsContext.Provider>
         </MemoryRouter>
     )

c02db6e78ad5fbf98a5063d8662239b7e3a3b604
StoyanBanov <sbanov05@gmail.com>
2023-10-02 14:25:23 +0300

    add test for catalog child categories

--- src/components/catalog/Catalog.test.js
+++ src/components/catalog/Catalog.test.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import { rest } from 'msw'
 import { setupServer } from 'msw/node'
-import { render, screen } from '@testing-library/react'
+import { render, screen, waitFor } from '@testing-library/react'
 import '@testing-library/jest-dom'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, MemoryRouter, Route, Routes } from 'react-router-dom'
 import { Catalog } from './Catalog'
 import { DimensionsContext } from '../common/context/DimensionsContext'
 import { AuthContext } from '../common/context/AuthContext'
@@ -16,7 +16,18 @@ const mockUser = {
 const mockParentCategories = [
     {
         _id: 1,
-        title: 'a'
+        title: 'clickMe',
+        childCategories: [
+            {
+                _id: 1,
+                parentCategory: 1,
+                title: 'a1cat'
+            }, {
+                _id: 2,
+                parentCategory: 1,
+                title: 'a2cat'
+            }
+        ]
     }, {
         _id: 2,
         title: 'b'
@@ -25,6 +36,9 @@ const mockParentCategories = [
 
 const host = 'http://localhost:3030'
 const server = setupServer(
+    rest.get(host + `/category/:id`, (req, res, ctx) => {
+        return res(ctx.json(mockParentCategories[0]))
+    }),
     rest.get(host + `/category?where=${encodeURIComponent('parentCategory=null')}`, (req, res, ctx) => {
         return res(ctx.json(mockParentCategories))
     })
@@ -42,14 +56,25 @@ test('loads parent categories', async () => {
     expect(titles.length).toBe(mockParentCategories.length)
 })
 
-function renderSkeleton(user) {
+test('loads child categories', async () => {
+    renderSkeleton(mockUser, `/${mockParentCategories[0].title}/${mockParentCategories[0]._id}`)
+
+    let titles = await Promise.all(mockParentCategories[0].childCategories.map(c => screen.findByText(c.title)))
+
+    expect(titles.length).toBe(mockParentCategories[0].childCategories.length)
+})
+
+function renderSkeleton(user, route = '') {
     render(
-        <BrowserRouter>
+        <MemoryRouter initialEntries={[`/catalog${route}`]}>
             <DimensionsContext.Provider value={{ windowWidth: 1000 }}>
                 <AuthContext.Provider value={{ user }}>
-                    <Catalog />
+                    <Routes>
+                        <Route path='catalog/:catTitle/:catId' element={<Catalog />} />
+                        <Route path='catalog' element={<Catalog />} />
+                    </Routes>
                 </AuthContext.Provider>
             </DimensionsContext.Provider>
-        </BrowserRouter>
+        </MemoryRouter>
     )
 }

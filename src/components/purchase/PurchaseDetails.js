c18bc1bb41e9e4cff77e36e6929aec8a8bf8cd9a
StoyanBanov <sbanov05@gmail.com>
2023-10-13 17:23:51 +0300

    bump purchase details

--- src/components/purchase/PurchaseDetails.js
+++ src/components/purchase/PurchaseDetails.js
@@ -3,6 +3,8 @@ import { useOutletContext } from "react-router-dom"
 import { addUserPurchase } from "../../data/services/userService"
 import { CartContext } from "../common/context/CartContext"
 
+import style from "./style.module.css"
+
 export const PurchaseDetails = () => {
     const { purchase } = useOutletContext()
 
@@ -15,9 +17,19 @@ export const PurchaseDetails = () => {
     }, [emptyCart, purchase])
 
     return (
-        <div>
-            address: {purchase.address}
-            <button onClick={CreatePurchase}>Create purchase</button>
+        <div className={style.purchaseDetailsContainer}>
+            <div>
+                <span>name: </span>
+                {purchase.firstName} {purchase.lastName}
+            </div>
+            <div>
+                <span>address: </span>
+                {purchase.address}
+            </div>
+
+            <div>
+                <button onClick={CreatePurchase}>Create purchase</button>
+            </div>
         </div>
     )
 }

44a50916e2dcc0899c234776662e30721530b889
StoyanBanov <sbanov05@gmail.com>
2023-10-18 12:58:54 +0300

    bump purchase list view

--- src/components/admin/purchase/PurchaseList.js
+++ src/components/admin/purchase/PurchaseList.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react"
 import { getAllNonVerifiedPurchases } from "../../../data/services/userService"
+import { HiddenSub } from "../../common/helpers/hiddenSub/HiddenSub"
 
 export const PurchaseList = () => {
     const [purchases, setPurchases] = useState([])
@@ -8,11 +9,13 @@ export const PurchaseList = () => {
         getAllNonVerifiedPurchases().then(setPurchases)
     }, [])
 
+    console.log(purchases[0]);
+
     return (
         <div>
             <ul>
                 {
-                    purchases.map(p => <li key={p._id}>{p.address}</li>)
+                    purchases.map(p => <li key={p._id}><HiddenSub title={p.createdOn}>{p.address}</HiddenSub></li>)
                 }
             </ul>
         </div>
---
+++ src/components/common/helpers/hiddenSub/HiddenSub.js
@@ -0,0 +1,37 @@
+import { useCallback, useEffect, useState } from 'react'
+import style from './style.module.css'
+
+export const HiddenSub = ({ children, title, initialVisibility = false }) => {
+    const [itemsVisibility, setItemsVisibility] = useState(false)
+
+    useEffect(() => {
+        setItemsVisibility(initialVisibility)
+    }, [initialVisibility])
+
+    const showNavItemHandler = useCallback(e => {
+        setItemsVisibility(state => !state)
+    }, [])
+
+    return (
+        <div style={itemsVisibility ? { color: 'black' } : { color: 'rgb(70, 70, 70)' }} className={style.hiddenSubContainer}>
+            <div onClick={showNavItemHandler} className={style.hiddenSubTop}>
+                <div>
+                    <span style={itemsVisibility ? { fontWeight: 'bold' } : {}}>{title}</span>
+                </div>
+
+                <svg width={18} height={18} fill="none" stroke="black" strokeWidth={2}>
+                    {itemsVisibility
+                        ? <path d={'M2 2 L9 14 L16 2'} />
+                        : <path d={'M12 2 L2 9 L12 16'} />
+                    }
+                </svg>
+            </div>
+
+            {itemsVisibility &&
+                <div className={style.hiddenSubBot}>
+                    {children}
+                </div>
+            }
+        </div>
+    )
+}

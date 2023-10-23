6f43f33129847c2e06998e8a3a9d141f86e320f7
StoyanBanov <sbanov05@gmail.com>
2023-10-23 19:29:50 +0300

    bump purchase info for admin

--- src/components/admin/purchase/purchaseListItem.js
+++ src/components/admin/purchase/purchaseListItem.js
@@ -1,4 +1,5 @@
 import { HiddenSub } from "../../common/helpers/hiddenSub/HiddenSub"
+import { Link } from 'react-router-dom'
 
 export const PurchaseListItem = ({ purchase, updatePurchaseListHandler }) => {
     const updatedPurchaseHandler = e => {
@@ -22,8 +23,15 @@ export const PurchaseListItem = ({ purchase, updatePurchaseListHandler }) => {
             >
                 <div>
                     <p><b>Recipient:</b> {purchase.firstName} {purchase.lastName}</p>
-                    <p><b>Address:</b> {purchase.address}</p>
+                    <p style={{ textTransform: "capitalize" }}><b>{purchase.deliverTo}:</b> {purchase.address}</p>
                     <p><b>Total sum:</b> {purchase.items.reduce((t, { item: { price }, count }) => t + price * count, 0)}$</p>
+
+                    <span><b>Products:</b></span>
+                    <ol>
+                        {
+                            purchase.items.map(({ item, count }) => <li key={item._id}><b>{item.count}</b> of <Link to={`/catalog/${item._id}`}><b>{item.title}</b></Link> for <b>{item.price * count}$</b></li>)
+                        }
+                    </ol>
                 </div>
             </HiddenSub>
         </li>

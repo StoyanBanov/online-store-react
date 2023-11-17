70a8efaa2bb38a9a3c4dfb37c309a2463b0ca406
StoyanBanov <sbanov05@gmail.com>
2023-11-17 20:33:38 +0200

    bump search results list

--- src/components/header/Search.js
+++ src/components/header/Search.js
@@ -72,10 +72,22 @@ export const Search = ({ autoFocus = false, closeHandler }) => {
             .then(setSearchResults)
     }, [searchValue])
 
+    const blurSearchHandler = useCallback(() => {
+        setSearchResults([])
+    }, [])
+
     return (
         <div className={style.searchBar}>
             <form onSubmit={submitHandler} onBlur={closeHandler && blurHandler}>
-                <input style={{ width }} value={searchValue} onChange={searchValueChangeHandler} onKeyUp={typeHandler} autoFocus={autoFocus} />
+                <input
+                    style={{ width }}
+                    value={searchValue}
+                    onChange={searchValueChangeHandler}
+                    onKeyUp={typeHandler}
+                    onBlur={blurSearchHandler}
+                    autoFocus={autoFocus}
+                />
+
                 <svg onClick={submitHandler} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M15.7955 15.8111L21 21M18 10.5C18 14.6421 14.6421 18 10.5 18C6.35786 18 3 14.6421 3 10.5C3 6.35786 6.35786 3 10.5 3C14.6421 3 18 6.35786 18 10.5Z" stroke="#000000" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"></path> </g></svg>
 
                 <ul className={style.searchBarResultsContainer}>

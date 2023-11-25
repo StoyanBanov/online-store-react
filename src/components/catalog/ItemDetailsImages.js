40a755069c13b1c0c0de94257b5ef48b893e1933
Stoyan Banov <sbanov05@gmail.com>
2023-11-25 19:54:08 +0200

    refactor item details

--- src/components/catalog/ItemDetailsImages.js
+++ src/components/catalog/ItemDetailsImages.js
@@ -4,6 +4,7 @@ import styleDetails from './itemDetails/style.module.css'
 import { useCarousel } from '../common/hooks/useCarousel'
 import { IMAGES_DIR } from '../../constants'
 import { CloseSVG } from '../common/svg/CloseSVG'
+import { IMAGE_OVERLAY_MOVE_LEFT, IMAGE_OVERLAY_MOVE_RiGHT } from './constants'
 
 export const ItemDetailsImages = ({ images }) => {
     const [imageIndex, setImageIndex] = useState(0)
@@ -26,6 +27,13 @@ export const ItemDetailsImages = ({ images }) => {
         if (e.target === e.currentTarget) setShowImageOverlay(false)
     }, [])
 
+    const imageOverlayChangeImageHandler = useCallback(direction => {
+        if (direction === IMAGE_OVERLAY_MOVE_LEFT)
+            setImageIndex(state => state === 0 ? images.length - 1 : state - 1)
+        else if (direction === IMAGE_OVERLAY_MOVE_RiGHT)
+            setImageIndex(state => state === images.length - 1 ? 0 : state + 1)
+    }, [images.length])
+
     return (
         <div className={style.itemImages}>
             {images &&
@@ -41,14 +49,14 @@ export const ItemDetailsImages = ({ images }) => {
                             </div>
 
                             <div onClick={imageOverlayCloseHandler} className={styleDetails.imageOverlayContainer}>
-                                <svg onClick={() => setImageIndex(state => state === 0 ? images.length - 1 : state - 1)} width={40} height={80} stroke='white' strokeWidth={2}>
+                                <svg onClick={imageOverlayChangeImageHandler(IMAGE_OVERLAY_MOVE_LEFT)} width={40} height={80} stroke='white' strokeWidth={2}>
                                     <line x1={2} y1={40} x2={38} y2={2} />
                                     <line x1={2} y1={40} x2={38} y2={78} />
                                 </svg>
 
                                 <img className={styleDetails.mainImage} src={`${IMAGES_DIR}/${images[imageIndex]}`} alt={images[imageIndex]} />
 
-                                <svg onClick={() => setImageIndex(state => state === images.length - 1 ? 0 : state + 1)} width={40} height={80} stroke='white' strokeWidth={2}>
+                                <svg onClick={imageOverlayChangeImageHandler(IMAGE_OVERLAY_MOVE_RiGHT)} width={40} height={80} stroke='white' strokeWidth={2}>
                                     <line x2={2} y1={40} x1={38} y2={2} />
                                     <line x2={2} y1={40} x1={38} y2={78} />
                                 </svg>
---
+++ src/components/catalog/constants.js
@@ -0,0 +1,2 @@
+export const IMAGE_OVERLAY_MOVE_LEFT = 'left'
+export const IMAGE_OVERLAY_MOVE_RiGHT = 'right'

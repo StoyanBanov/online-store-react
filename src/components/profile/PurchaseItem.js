d6c3e27df431325f71611b384e39a4e1932b5c83
Stoyan Banov <sbanov05@gmail.com>
2023-11-29 18:35:05 +0200

    bump user purchases list style

--- src/components/profile/PurchaseItem.js
+++ src/components/profile/PurchaseItem.js
@@ -2,12 +2,12 @@ import { Link } from "react-router-dom"
 
 export const PurchaseItem = ({ itemObj }) => {
     return (
-        <p key={itemObj.item._id}>
+        <li key={itemObj.item._id}>
             {itemObj.count}
             <span> of </span>
             <Link to={`/catalog/${itemObj.item._id}`}>{itemObj.item.title}</Link>
             <span> for </span>
             {(itemObj.item.price * itemObj.count).toFixed(2)}$
-        </p>
+        </li>
     )
 }
--- src/components/profile/UserPurchases.js
+++ src/components/profile/UserPurchases.js
@@ -8,7 +8,14 @@ export const UserPurchases = () => {
         <div>
             <ul>
                 {
-                    purchases?.map(p => <li key={p._id}>{p.items.map(i => <PurchaseItem key={i.item._id} itemObj={i} />)} Address: {p.address}</li>)
+                    purchases?.map(p =>
+                        <li key={p._id}>
+                            <ol>
+                                {p.items.map(i => <PurchaseItem key={i.item._id} itemObj={i} />)}
+                            </ol>
+
+                            <p>Address: {p.address}</p>
+                        </li>)
                 }
             </ul>
         </div>

b460162354df939ad6078c7b2c5694860e620662
Stoyan Banov <sbanov05@gmail.com>
2024-01-06 21:33:28 +0200

    refactor pagination

--- src/components/common/helpers/PaginationPages.js
+++ src/components/common/helpers/PaginationPages.js
@@ -5,8 +5,8 @@ export const PaginationPages = ({ currentPage, totalPages, pageWrap: PageWrap })
         makePages(currentPage, totalPages)
             .map((p, i) => !isNaN(p)
                 ? PageWrap
-                    ? <PageWrap key={`${p}${i}`} page={p}>{currentPage === p ? <strong>{p}</strong> : p}</PageWrap>
-                    : <span key={`${p}${i}`} page={p}>{currentPage === p ? <strong>{p}</strong> : p}</span>
+                    ? <PageWrap key={`${p}${i}`} page={p}>{currentPage === p ? <b>{p}</b> : p}</PageWrap>
+                    : <span key={`${p}${i}`} page={p}>{currentPage === p ? <b>{p}</b> : p}</span>
                 : <span key={`${p}${i}`} >{p}</span>
             )
     )

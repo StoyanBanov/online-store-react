970e379225a86e01c4c3bb5a6132ee632ffd4cba
Stoyan Banov <sbanov05@gmail.com>
2024-01-07 21:35:36 +0200

    fix pop component

--- src/components/common/helpers/popBefore/PopBefore.js
+++ src/components/common/helpers/popBefore/PopBefore.js
@@ -6,7 +6,7 @@ export const PopBefore = ({ popRef, displayPopClickHandler, children, closeBtnTe
             <div onClick={() => displayPopClickHandler(false)} className={style.popBefore} />
             <div ref={popRef} className={style.popContainer}>
                 <div className={style.popHeader}>
-                    <svg id="catClose" onClick={() => displayPopClickHandler(false)} width={30} height={30} stroke="black" strokeWidth={2}>
+                    <svg onClick={() => displayPopClickHandler(false)} width={30} height={30} stroke="black" strokeWidth={2}>
                         <line x1={2} y1={2} x2={28} y2={28} />
                         <line x1={2} y1={28} x2={28} y2={2} />
                     </svg>
